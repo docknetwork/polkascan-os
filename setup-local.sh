67cc12eac01cd73e3f11cd984dc86034b90ee2e5
Sam Hellawell <sshellawell@gmail.com>
2020-08-12 22:00:33 +0100

    fix local scripts

--- setup-local.sh
+++ setup-local.sh
@@ -1,4 +1,4 @@
-export DOCKERHOST=3.128.224.235
+export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 docker-compose -p dockpoa -f docker-compose.dock.yml up -d mysql
 
 echo "waiting some time to let mysql setup"
